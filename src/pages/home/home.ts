35e48ed7ff898be598e651d41aa33bc15fa72966
testgitconfig <testgitconfig>
2017-09-25 14:29:38 +0700

    test option navigator

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,23 +2,29 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator';
 
+declare var google;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private launchNavigator: LaunchNavigator) {
+  destination:string;
+  start:string;
 
+  constructor(public navCtrl: NavController, private launchNavigator: LaunchNavigator) {
+    this.start = '';
+    this.destination = 'Westminster, London, UK';
   }
 
   openApp(){
     let options: LaunchNavigatorOptions = {
-      start: '',
+      start: this.start,
       //app: LaunchNavigator.APPS.UBER
     };
     
-    this.launchNavigator.navigate('Toronto, ON', options)
+    this.launchNavigator.navigate(this.destination, options)
       .then(
         success => console.log('Launched navigator'),
         error => console.log('Error launching navigator', error)
